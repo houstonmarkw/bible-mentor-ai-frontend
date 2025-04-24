baf62e57e7e2ec6cdd1e3f11dd8bd4cc495b51ac
Mark Houston <markhouston@Marks-MacBook-Air-2.local>
2025-04-23 20:29:58 -0600

    Fix RichTextEditor casing for Vercel deploy

---
+++ src/components/RichTextEditor.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useEffect } from 'react'
+
+type Props = {
+  content: string
+  onChange: (content: string) => void
+}
+
+export default function RichTextEditor({ content, onChange }: Props) {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML())
+    },
+  })
+
+  useEffect(() => {
+    if (editor && content !== editor.getHTML()) {
+      editor.commands.setContent(content)
+    }
+  }, [content, editor])
+
+  return (
+    <div className="border rounded px-3 py-2 text-blue-800 bg-white shadow">
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
